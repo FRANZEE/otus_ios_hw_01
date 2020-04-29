caecd634c4e2750a4d4f90f6882ca4b3c25f7939
Alex <franzee.franz@gmail.com>
2020-04-29 05:57:50 +0300

    added tabs

--- swiftui_dz_01/ContentView.swift
+++ swiftui_dz_01/ContentView.swift
@@ -9,8 +9,51 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var showModal = false
+    @State private var selection = 1
+    
     var body: some View {
-        Text("Hello World")
+        TabView(selection: $selection) {
+            Button(action: {
+                self.selection = 1
+            }) {
+               // 2.
+               //self.showModal.toggle()
+                Text("Show list item")
+            // 3.
+            }
+                .tabItem {
+                    Image(systemName: "1.square.fill")
+                    Text("First")
+            }.tag(0)
+
+            NavigationView {
+                List {
+                    ForEach(0...9, id: \.self) { row in
+                        NavigationLink(destination: DetailView()) {
+                            Text("Show Detail View from \(row)")
+                        }.navigationBarTitle("Navigation")
+                    }
+                }
+            }.tabItem {
+                Image(systemName: "2.square.fill")
+                Text("Second")
+            }.tag(1)
+            
+            Button("Show Modal") {
+               // 2.
+               self.showModal.toggle()
+            // 3.
+            }.sheet(isPresented: $showModal) {
+                 ModalView(showModal: self.$showModal)
+             }.tabItem {
+                Image(systemName: "3.square.fill")
+                Text("Third")
+            }.tag(2)
+                
+        }
+        .font(.headline)
     }
 }
 
